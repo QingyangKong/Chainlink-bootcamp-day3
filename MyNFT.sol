6d7662bdddb92d91f95549716957cd2f678eda1d
Frank Kong <frankkongnj@gmail.com>
2023-10-14 14:15:36 +0800

    make nft stick with 0.8.19

--- MyNFT.sol
+++ MyNFT.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyToken is ERC721URIStorage, Ownable {
+    string constant public TOKEN_URI = "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
+    uint256 counter;
+
     constructor() ERC721("MyNFT", "MN") {}
-    string constant TOKEN_URI = "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
-    uint256 public tokenId;
 
-    function safeMint(address to)
-        public
-    {
+    function safeMint(address to) public {
+        uint256 tokenId = counter;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, TOKEN_URI);
-        tokenId++;
+        counter++;
     }
 }
